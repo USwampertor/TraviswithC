e915c3ffb70f0eed8cf87cf3dee1deceb6e30c59
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2018-11-27 17:32:45 -0600

    *gtest.cmake

--- gtest.cmake
+++ gtest.cmake
@@ -1,4 +1,4 @@
-set(GOOGLETEST_ROOT googletest CACHE STRING "Google Test source root")
+set(GOOGLETEST_ROOT ./googletest/googletest CACHE STRING "Google Test source root")
 
 include_directories(SYSTEM
     ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}
@@ -7,7 +7,8 @@ include_directories(SYSTEM
 
 set(GOOGLETEST_SOURCES
     ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc)
+    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
+    )
 
 foreach(_source ${GOOGLETEST_SOURCES})
     set_source_files_properties(${_source} PROPERTIES GENERATED 1)
