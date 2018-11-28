232cd9e18ed7875dbf1ade89ac0b0c9f3deb0ec7
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2018-11-27 23:47:43 -0600

    sss

--- main.cpp
+++ main.cpp
@@ -21,12 +21,16 @@ struct Fixture : public ::testing::Test {
 int main(int argc, char **argv) {
 
   printf("Hello from Travis C output \n");
-  ::testing::InitGoogleTest(&argc,argv);
-  RUN_ALL_TESTS();
-  std::cout << fails;
+  testing::InitGoogleTest(&argc,argv);
+  return RUN_ALL_TESTS();
 }
 
 TEST_F(Fixture, bar) {
-    EXPECT_EQ(1,1);
-    fails += ::testing::Test::HasFailure();
+  EXPECT_EQ(1,1);
+  fails += ::testing::Test::HasFailure();
+}
+
+TEST_F(Fixture, foo) {
+  EXPECT_EQ(1,0);
+  fails += ::testing::Test::HasFailure();
 }
