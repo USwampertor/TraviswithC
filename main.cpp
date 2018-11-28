6e97ef7596f7f199a26cde5b48ec12a9c7661a74
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2018-11-27 20:37:08 -0600

    *gtest

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <cstdio>
 #include <gtest/gtest.h>
 using namespace std;
-int main() {
-    printf("Hello from Travis C output \n");
-    return 0;
+int main(int argc, char **argv) {
+  printf("Hello from Travis C output \n");
+  ::testing::InitGoogleTest(&argc,argv);
+  ::testing::GTEST_FLAG(filter) = "Basic_Test*";
+  return RUN_ALL_TESTS();
+}
+
+TEST(Math, Basic_Test) {
+  EXPECT_TRUE(1 == 1);
 }
