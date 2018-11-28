f19533a2d9446290bd7015681897ba83aa0fc53a
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2018-11-27 20:40:45 -0600

    +gtest

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(int argc, char **argv) {
   printf("Hello from Travis C output \n");
   ::testing::InitGoogleTest(&argc,argv);
-  ::testing::GTEST_FLAG(filter) = "Basic_Test*";
   return RUN_ALL_TESTS();
 }
 
